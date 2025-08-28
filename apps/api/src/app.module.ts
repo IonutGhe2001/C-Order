a6ebbc4b3ca492def1346f6822331bf354712090
IonutGhe2001 <ionutgheba2001@gmail.com>
2025-08-28 14:59:02 +0300

    chore: remove suppliers module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
-import { SuppliersModule } from './suppliers/suppliers.module';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [AuthModule, UsersModule, TasksModule, SuppliersModule],
+  imports: [AuthModule, UsersModule, TasksModule],
   providers: [PrismaService],
 })
 export class AppModule {}
