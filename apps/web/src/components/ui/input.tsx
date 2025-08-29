e2cda3d9960a5bd8e7237824a42b26d44d9e3e75
IonutGhe2001 <ionutgheba2001@gmail.com>
2025-08-29 13:40:21 +0300

    refactor: remove tailwind apply

--- apps/web/src/components/ui/input.tsx
+++ apps/web/src/components/ui/input.tsx
@@ -5,9 +5,9 @@ const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLI
   ({ className, type, ...props }, ref) => {
     return (
       <input
-        type={type}
-            className={cn(
-            'input-base flex h-10 w-full bg-white px-3 py-2 text-sm ring-offset-background placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          type={type}
+          className={cn(
+            'flex h-10 w-full rounded-lg border bg-white px-3 py-2 text-sm ring-offset-background placeholder:text-gray-500 hover:border-brand focus:ring-2 focus-visible:outline-none focus-visible:ring-offset-2 ring-brand disabled:cursor-not-allowed disabled:opacity-50',
             className
           )}
         ref={ref}
--- apps/web/src/components/ui/textarea.tsx
+++ apps/web/src/components/ui/textarea.tsx
@@ -4,9 +4,9 @@ import { cn } from '@/lib/utils'
 const Textarea = React.forwardRef<HTMLTextAreaElement, React.TextareaHTMLAttributes<HTMLTextAreaElement>>(
   ({ className, ...props }, ref) => {
     return (
-      <textarea
-            className={cn(
-            'input-base flex min-h-[80px] w-full bg-white px-3 py-2 text-sm ring-offset-background placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        <textarea
+          className={cn(
+            'flex min-h-[80px] w-full rounded-lg border bg-white px-3 py-2 text-sm ring-offset-background placeholder:text-gray-500 hover:border-brand focus:ring-2 focus-visible:outline-none focus-visible:ring-offset-2 ring-brand disabled:cursor-not-allowed disabled:opacity-50',
             className
           )}
         ref={ref}
