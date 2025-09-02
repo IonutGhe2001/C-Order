513f4f7987441052573fff278def8103aec56124
IonutGhe2001 <ionutgheba2001@gmail.com>
2025-09-02 12:01:37 +0300

    fix: prebundle warning dep

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
+    include: ['warning'],
     exclude: [
       '@cyntler/react-doc-viewer',
       'react-pdf',
